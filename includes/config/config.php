1f7b3b6196eae7cd80e6f4fb707f6e858f73cc72
Seiron92 <42301363+Seiron92@users.noreply.github.com>
2019-10-04 20:41:06 +0200

    first commit

---
+++ includes/config/config.php
@@ -0,0 +1,13 @@
+<?php
+error_reporting(-1);
+ini_set("display_errors" , 1);
+function __autoload($class_name){
+    include $class_name . ".php";
+    }
+
+define("DBHOST", "localhost");
+define("DBUSER", "root");
+define("DBPASS", ""); 
+define("DBDATABASE", "courses");
+
+?>
